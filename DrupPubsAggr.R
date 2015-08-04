fdf25cb4922da75eecf6c8aa834c487a29a218ba
pyrgns <pyuergens@gmail.com>
2015-08-04 12:59:52 -0400

    Almost refactored
    one little bug with contributors

--- DrupPubsAggr.R
+++ DrupPubsAggr.R
@@ -4,13 +4,15 @@
 # R or other statistical computing software
 # 
 
+
 #
 # CONFIGURATION
 #
 
+# your working directory path
 working_directory = "/Users/pdy/code/R/Publications/RPubsScripts"
 
-#setting the working directory
+# setting the working directory
 setwd(file.path(working_directory))
 
 # setting the path to the csv of the main biblio table
@@ -23,7 +25,7 @@ types_path = "data/biblio_types.csv"
 contributor_path = "data/biblio_contributor.csv"
 
 # biblio_contributor_data table path
-contributor_data_path = "data/biblio_contributor.csv"
+contributor_data_path = "data/biblio_contributor_data.csv"
 
 # biblio_keyword table path
 keyword_path = "data/biblio_keyword.csv"
@@ -34,7 +36,9 @@ keyword_data_path = "data/biblio_keyword_data.csv"
 # biblio_pubmed table path
 pubmed_path = "data/biblio_pubmed.csv"
 
-## READING THE CSV TABLES INTO DATA FRAMES
+#
+# READING THE CSV TABLES INTO DATA FRAMES
+#
 
 # biblio table 
 biblio<-read.csv(biblio_path, head=TRUE, sep=",")
@@ -45,7 +49,7 @@ types<-read.csv(types_path, head=TRUE, sep=",")
 
 # biblio_contributor table, a join or juntion table between the biblio and biblio_contributor_data tables
 # Generally, there is a one one biblio : many contributors relatioinshipo 
-contributor<-read.csv(contributor__path, head=TRUE, sep=",")
+contributor<-read.csv(contributor_path, head=TRUE, sep=",")
 
 # biblio_contributor_data, a lookup table for contributor names 
 contributor_data<-read.csv(contributor_data_path, head=TRUE, sep=",")
@@ -54,63 +58,52 @@ contributor_data<-read.csv(contributor_data_path, head=TRUE, sep=",")
 # generally, 1 biblio: many keywords 
 keyword<-read.csv(keyword_path, head=TRUE, sep=",")
 
-
 # biblio_keywords, a lookup table for keyword names
 keyword_data<-read.csv(keyword_data_path, head=TRUE, sep=",")
-
-
 
 # biblio_pubmed table, a lookup table for pubmed and pubmed central ids
 # one biblio: one pubmed AND/OR pmc id(s)  
 pubmed<-read.csv(pubmed_path, head=TRUE, sep=",")
 
-# MERGING PUBMED AND TYPES INTO BIBLIO - ONE TO ONE RELATIONSHIPS 
+# Merging publication types into the biblio table - ONE TO ONE RELATIONSHIPS 
 
-#types
 biblio<-merge(biblio,types, by.x = "biblio_type", by.y = "tid", all=TRUE)
 
-#changing the column name from "name" to "type"
-# colnames(biblio)[7] <- "type"
-
-biblio$biblio_type <- NULL
-
-#pubmed
+# Merging pubmed PMC id's into biblio table
 biblio<-merge(biblio,pubmed, by.x= "nid", by.y = "nid", all=TRUE)
 
 # MERGING AND COLLAPSING KEYWORDS AND CONTRIBUTORS THEN MERGING INTO BIBLIO - MOST LIKELY TO TO BE ONE TO MANY RELATIONSHIPS
 
-
-# I'm using this for aggregate
-# http://stackoverflow.com/questions/16596515/aggregating-by-unique-identifier-and-concatenating-related-values-into-a-string
-
-
 # contributor
 
-c_merged<-merge(contributor,contributor_data, by = "cid", all=TRUE)
+contributor_merged<-merge(contributor,contributor_data, by = "cid", all=TRUE)
 
-c_merged$cid <- NULL
+contributor_merged$cid <- NULL
 
-c_agg<-aggregate(name~nid,paste,collapse=" ",data=c_merged)
+contributor_aggregate<-aggregate(name~nid,paste,collapse=" ",data=contributor_merged)
 
-biblio<-merge(biblio,c_agg, by.x= "nid", by.y = "nid", all=TRUE)
-
+biblio<-merge(biblio,contributor_aggregate, by.x= "nid", by.y = "nid", all=TRUE)
 
 # keywords
 
-k_merged<-merge(keyword,k_data, by = "kid", all=TRUE)
+keyword_merged<-merge(keyword,keyword_data, by = "kid", all=TRUE)
 
-k_merged$kid <- NULL
+keyword_merged$kid <- NULL
 
-k_agg<-aggregate(word~nid,paste,collapse=", ",data=k_merged)
+keyword_aggregate<-aggregate(word~nid,paste,collapse=", ",data=keyword_merged)
 
-biblio<-merge(biblio,k_agg, by.x= "nid", by.y = "nid", all=TRUE)
+biblio<-merge(biblio,keyword_aggregate, by.x= "nid", by.y = "nid", all=TRUE)
 
-# fixing the column names and merging the journals
-
-# biblio[is.null(biblio)] <- NA
-
+# Merging the two columns that could contain the journal name
 biblio<-transform(biblio,journal=interaction(biblio_secondary_title,biblio_tertiary_title,sep=' '))
 
+
+#
+# CLEAN UP
+#
+
+
+# Renaming the columns
 biblio$biblio_secondary_title <- NULL
 biblio$biblio_tertiary_title <- NULL
 colnames(biblio)[1] <- "id"
@@ -125,6 +118,12 @@ colnames(biblio)[9] <- "Authors"
 colnames(biblio)[10] <- "Keywords"
 colnames(biblio)[11] <- "Journal"
 
+# Reordering the columns
 biblio<-biblio[c("id","Title","Year","Date","Journal","Publisher","Type","Authors","Keywords","Pubmed ID", "PMCID")]
 
-write.csv(biblio, "BiblioAgg.csv")
+
+#
+# EXPORT
+#
+
+write.csv(biblio, "DrupalBiblioAggregate.csv")
